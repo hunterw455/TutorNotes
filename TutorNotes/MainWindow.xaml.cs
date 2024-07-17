f26130c05550c3a9e0ad8f8de5d639fa815b2014
hunterw455 <157230944+hunterw455@users.noreply.github.com>
2024-07-17 11:32:38 -0500

    Add project files.

---
+++ TutorNotes/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TutorNotes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void loginBttn_Click(object sender, RoutedEventArgs e)
+        {
+            // Add functions and capabilities that take care of password authentication
+
+
+
+            // Will load in new window once user inputs valid login credentials
+            HomeScreen homeScreen = new HomeScreen();
+            this.Visibility = Visibility.Hidden;
+            homeScreen.Show();
+        }
+        private void createAccount_MouseEnter(object sender, MouseEventArgs e)
+        {
+            createAccountTxt.Foreground = Brushes.Purple;
+            createAccountTxt.FontWeight = FontWeights.Bold;
+        }
+
+        private void createAccountTxt_MouseLeave(object sender, MouseEventArgs e)
+        {
+            SolidColorBrush customBrush = (SolidColorBrush)FindResource("CustomColor1");
+            createAccountTxt.Foreground = customBrush;
+            createAccountTxt.FontWeight = FontWeights.Light;
+        }
+
+        private void forgotPasswordTxt_MouseEnter(object sender, MouseEventArgs e)
+        {
+            forgotPasswordTxt.Foreground = Brushes.Purple;
+            forgotPasswordTxt.FontWeight= FontWeights.Bold;
+        }
+
+        private void forgotPasswordTxt_MouseLeave(object sender, MouseEventArgs e)
+        {
+            SolidColorBrush customBrush = (SolidColorBrush)FindResource("CustomColor1");
+            forgotPasswordTxt.Foreground= customBrush;
+            forgotPasswordTxt.FontWeight = FontWeights.Light;
+        }
+    }
+}
